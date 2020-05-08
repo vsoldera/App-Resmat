e10eba755b4916edba18e1aa6b458ba6291be53e
VictorSoldera <victorsolderalf@gmail.com>
2020-05-08 17:59:14 -0300

    Botões e Navegação por páginas

---
+++ resmat/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'nextRoute.dart';
+void main() {
+  runApp(
+    MaterialApp(
+      home: Home(), //run the home page of the application
+    ),
+  );
+}
+
